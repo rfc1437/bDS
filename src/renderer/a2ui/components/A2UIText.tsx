a5089814008ef3e6a1211221ae5db683000b6148
hugo <hugoms@me.com>
2026-03-01 20:10:25 +0100

    Block external images in chat markdown renderer (CSP compliance)

--- src/renderer/a2ui/components/A2UIText.tsx
+++ src/renderer/a2ui/components/A2UIText.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { type ReactNode } from 'react';
 import Markdown from 'marked-react';
 import type { A2UIResolvedComponent, A2UIClientAction } from '../../../main/a2ui/types';
 
@@ -10,7 +10,16 @@ interface A2UIComponentProps {
   renderChildren?: (children: A2UIResolvedComponent[]) => React.ReactNode;
 }
 
+const safeRenderer = {
+  image(src: string, alt: string): ReactNode {
+    if (/^https?:\/\//i.test(src)) {
+      return <a href={src} key={src} title={alt}>{alt || src}</a>;
+    }
+    return <img src={src} alt={alt} key={src} />;
+  },
+};
+
 export const A2UIText: React.FC<A2UIComponentProps> = ({ component }) => {
   const text = String(component.properties.text ?? '');
-  return <Markdown>{text}</Markdown>;
+  return <Markdown renderer={safeRenderer}>{text}</Markdown>;
 };
